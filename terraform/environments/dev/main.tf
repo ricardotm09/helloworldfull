318d19524e2bb14f06c4c143cdcb2ffef88d9899
Ricardo <ricardotm9@outlook.com>
2026-07-27 20:09:10 -0600

    TF oidc issuer enabled to true

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -52,6 +52,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = var.aks_dns_prefix
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name       = "system"
